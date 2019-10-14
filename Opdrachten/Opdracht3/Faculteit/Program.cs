bed087380b6e4a1974e545c2938d5fae5c93b8c7
nmdgent-larissagyselinck <larigyse@student.arteveldehs.be>
2019-10-14 12:40:35 +0200

    Faculteit & Fibonacci

---
+++ Opdrachten/Opdracht3/Faculteit/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Faculteit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // Faculteit
+
+            int faculteit = 1;
+            int i;
+            int getal;
+            
+            Console.WriteLine("Geef een getal in: "); 
+
+            getal = Convert.ToInt32(Console.ReadLine());
+
+            for(i = 1; i <= getal; i++){      
+                faculteit = faculteit * i;      
+            }
+
+            Console.WriteLine("Faculteit van " + getal + " is: " + faculteit);
+        }
+    }
+}
