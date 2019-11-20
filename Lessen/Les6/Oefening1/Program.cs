c391cfd52de67799b857366c79deebf436036de7
nmdgent-larissagyselinck <larigyse@student.arteveldehs.be>
2019-11-20 17:04:40 +0100

    WIP

---
+++ Lessen/Les6/Oefening1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Oefening1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            object o = null;
+            try
+            {
+                int i = (int)o;     // error
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine("NRE: " + e.Message);
+            }
+            catch (InvalidCastException e)
+            {
+                Console.Write("ICE: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("E: " + e.StackTrace);
+            }
+            finally
+            {
+                Console.WriteLine("Finally");
+            }
+        }
+    }
+}
